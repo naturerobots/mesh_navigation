465c2ed098024ce0efa97a4b1bb05b95654a78fc
Sebastian Pütz <spuetz@uos.de>
2019-02-20 17:39:54 +0100

    wip mesh_navigation

---
+++ mesh_map/include/mesh_map/mesh_map.h
@@ -0,0 +1,64 @@
+/*
+ *  Copyright 2019, Sebastian Pütz
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *
+ *  3. Neither the name of the copyright holder nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  authors:
+ *    Sebastian Pütz <spuetz@uni-osnabrueck.de>
+ *
+ */
+
+#ifndef MESH_NAVIGATION__MESH_MAP_H
+#define MESH_NAVIGATION__MESH_MAP_H
+
+#include <lvr2/io/HDF5IO.hpp>
+#include <lvr2/geometry/BaseVector.hpp>
+
+namespace mesh_map{
+
+using BaseVec = lvr2::BaseVector<float>;
+
+class MeshMap
+{
+ public:
+  MeshMap(const std::string& mesh_map, const std::string& mesh_part);
+
+ private:
+  std::shared_ptr<lvr2::AttributeMeshIOBase> mesh_io_ptr;
+  lvr2::HalfEdgeMesh<BaseVec> mesh;
+
+
+
+};
+
+} /* namespace mesh_map */
+
+
+#endif //MESH_NAVIGATION__MESH_MAP_H
---
+++ mesh_map/src/mesh_map.cpp
@@ -0,0 +1,65 @@
+/*
+ *  Copyright 2019, Sebastian Pütz
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *
+ *  3. Neither the name of the copyright holder nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  authors:
+ *    Sebastian Pütz <spuetz@uni-osnabrueck.de>
+ *
+ */
+#include <mesh_map/mesh_map.h>
+#include <lvr2/io/HDF5IO.hpp>
+#include <ros/ros.h>
+
+namespace mesh_map{
+
+  MeshMap::MeshMap(const std::string& mesh_map, const std::string& mesh_part)
+  {
+    ROS_INFO_STREAM("Reading the mesh map...");
+    mesh_io_ptr = std::shared_ptr<lvr2::AttributeMeshIOBase>(new lvr2::HDF5IO(mesh_map, mesh_part));
+    mesh_opt = mesh_io_ptr->getMesh();
+
+    if(mesh_opt)
+    {
+      mesh = mesh_opt.get();
+    }
+    else
+    {
+      ROS_ERROR_STREAM("Could not load mesh map!");
+    }
+    
+  }
+
+} /* namespace mesh_map */
+
+
+
+
+
