5c54f373788eb0cb0c404004f69eb151ebeae415
Malte kl. Piening <mkl.piening@gmail.com>
2020-01-28 21:29:15 +0100

    replaced cpp-httplib with curl because of strange crashes of the library and added support for the new rest api

--- mesh_client/include/mesh_client/mesh_client.h
+++ mesh_client/include/mesh_client/mesh_client.h
@@ -2,50 +2,46 @@
 #ifndef MESH_CLIENT_H_
 #define MESH_CLIENT_H_
 
+//  #define CPPHTTPLIB_OPENSSL_SUPPORT
+
 #include <string>
 #include <array>
 #include <lvr2/io/AttributeMeshIOBase.hpp>
+#include <lvr2/geometry/BoundingBox.hpp>
+#include <lvr2/geometry/BaseVector.hpp>
 #include <jsoncpp/json/json.h>
-#include "cpp-httplib/httplib.h"
 
-namespace mesh_client{
-class MeshClient : public lvr2::AttributeMeshIOBase {
-
+namespace mesh_client
+{
+class MeshClient : public lvr2::AttributeMeshIOBase
+{
 public:
   /**
    * @brief Constructs a mesh client which receaves
-   * @param srv_address The address of the server, use localhost if the server is used locally.
-   * @param srv_port The server's port, default is 8080
-   * @param srv_endpoint, the servers endpoint, e.g., "/mesh"
+   * @param srv_url The url of the server, use http://localhost/my/path/to/mesh if the server is used locally.
    */
-  MeshClient(
-      const std::string &srv_address,
-      const unsigned int &srv_port,
-      const std::string &srv_endpoint);
+  MeshClient(const std::string& srv_url);
 
   /**
    * @brief sets the Bounding box for the query which is send to the server
    */
-  void setBoundingBox(
-      const float min_x, const float min_y, const float min_z,
-      const float max_x, const float max_y, const float max_z
-      );
+  void setBoundingBox(float min_x, float min_y, float min_z, const float max_x, const float max_y, const float max_z);
+  void addFilter(std::string channel, float min_value, float max_value);
 
   /**
    * @brief Builds a JSON string containing the set bounding
    *        box and the attribute name and the attribute group
    * @param attribute_name The name of the corresponding attribute
    *        to be requested from the server
-   * @param attribute_group The name of the group of the attribute
-   *        (vertex_attribute, face_attribute, ...)
    * @return the compsoed JSON string following the server's specification
    */
-  std::string buildJson(const std::string& attribute_name, const std::string& attribute_group = "");
+  std::string buildJson(const std::string& attribute_name);
 
   /**
    * @brief Defines the data type of the transferred channel to decode the byte buffer.
    */
-  enum Type : char{
+  enum Type : char
+  {
     UINT = 0,
     FLOAT = 1,
     UCHAR = 2
@@ -102,7 +98,6 @@ public:
    */
   bool getChannel(const std::string group, const std::string name, lvr2::UCharChannelOptional& channel);
 
-
   /**
    * @brief addChannel  Writes a float attribute channel from the given group with the given name
    * @param group       The associated attribute group
@@ -131,20 +126,24 @@ public:
   bool addChannel(const std::string group, const std::string name, const lvr2::UCharChannel& channel);
 
 private:
+  std::unique_ptr<std::string> requestChannel(std::string channel);
 
   std::map<std::string, lvr2::UCharChannel> uchar_channels;
   std::map<std::string, lvr2::IndexChannel> index_channels;
   std::map<std::string, lvr2::FloatChannel> float_channels;
 
-  std::string srv_address;
-  std::string srv_endpoint;
-  unsigned int srv_port;
-  std::array<float, 6> bounding_box;
-  httplib::Client http_client;
-  Json::Value json_bb;
+  std::string server_url_;
+
+  std::array<float, 6> bounding_box_;
+  std::map<std::string, std::pair<float, float>> mesh_filters_;
 };
 
-
+size_t writeFunction(void* ptr, size_t size, size_t nmemb, std::string* data)
+{
+  data->append((char*)ptr, size * nmemb);
+  return size * nmemb;
 }
 
-#endif // MESH_CLIENT_H_
+}  // namespace mesh_client
+
+#endif  // MESH_CLIENT_H_
