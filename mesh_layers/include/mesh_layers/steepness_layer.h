5a3608bef4c051abde05b9811d37aff3b94114b7
Malte kl. Piening <mkl.piening@gmail.com>
2020-11-10 18:27:25 +0100

    added in code documentation gi mesh_layers and dijkstra_mesh_planner

--- mesh_layers/include/mesh_layers/steepness_layer.h
+++ mesh_layers/include/mesh_layers/steepness_layer.h
@@ -44,44 +44,109 @@
 
 namespace mesh_layers
 {
+/**
+ * @brief Costmap layer which calculates the steepness of the vertices. This is useful to avoid stairs
+ */
 class SteepnessLayer : public mesh_map::AbstractLayer
 {
+  /**
+   * @brief try read layer from map file
+   *
+   * @return true if successul; else false
+   */
   virtual bool readLayer();
 
+  /**
+   * @brief try to write layer to map file
+   *
+   * @return true if successfull; else false
+   */
   virtual bool writeLayer();
 
+  /**
+   * @brief delivers the threshold above which vertices are marked lethal
+   *
+   * @return lethal threshold
+   */
   virtual float threshold();
 
+  /**
+   * @brief delivers the default layer value
+   *
+   * @return default value used for this layer
+   */
   virtual float defaultValue()
   {
     return std::numeric_limits<float>::infinity();
   }
 
+  /**
+   * @brief calculate the values of this layer
+   *
+   * @return true if successfull; else false
+   */
   virtual bool computeLayer();
 
+  /**
+   * @brief mark vertices with values above the threshold as lethal
+   *
+   * @return true if successfull; else false
+   */
   bool computeLethals();
 
+  /**
+   * @brief deliver the current costmap
+   *
+   * @return calculated costmap
+   */
   virtual lvr2::VertexMap<float>& costs();
 
+  /**
+   * @brief deliver set containing all vertices marked as lethal
+   *
+   * @return lethal vertices
+   */
   virtual std::set<lvr2::VertexHandle>& lethals()
   {
     return lethal_vertices;
   }
 
+  /**
+   * @brief update set of lethal vertices by adding and removing vertices
+   *
+   * @param added_lethal vertices to be marked as lethal
+   * @param removed_lethal vertices to be removed from the set of lethal vertices
+   */
   virtual void updateLethal(std::set<lvr2::VertexHandle>& added_lethal, std::set<lvr2::VertexHandle>& removed_lethal){};
 
-  lvr2::DenseVertexMap<float> steepness;
-
+  /**
+   * @brief initializes this layer plugin
+   *
+   * @param name name of this plugin
+   *
+   * @return true if initialization was successfull; else false
+   */
   virtual bool initialize(const std::string& name);
 
+  // latest costmap
+  lvr2::DenseVertexMap<float> steepness;
+  // set of all current lethal vertices
   std::set<lvr2::VertexHandle> lethal_vertices;
 
   // Server for Reconfiguration
   boost::shared_ptr<dynamic_reconfigure::Server<mesh_layers::SteepnessLayerConfig>> reconfigure_server_ptr;
   dynamic_reconfigure::Server<mesh_layers::SteepnessLayerConfig>::CallbackType config_callback;
+  // true if the first reconfigure config has been received; else false
   bool first_config;
+  // current reconfigure config
   SteepnessLayerConfig config;
 
+  /**
+   * @brief callback for incoming reconfigure configs
+   *
+   * @param cfg new config
+   * @param level level
+   */
   void reconfigureCallback(mesh_layers::SteepnessLayerConfig& cfg, uint32_t level);
 };
 
