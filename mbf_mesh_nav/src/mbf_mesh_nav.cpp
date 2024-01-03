4d0dc8f8b5e046cebd41f6cb0780aa77a7084fc1
Matthias Holoch <mholoch@gmail.com>
2024-01-03 15:49:52 +0100

    port mbf mesh nav executable

--- mbf_mesh_nav/src/mbf_mesh_nav.cpp
+++ mbf_mesh_nav/src/mbf_mesh_nav.cpp
@@ -38,37 +38,35 @@
 #include <mbf_utility/types.h>
 #include <signal.h>
 #include <tf2_ros/transform_listener.h>
+#include <rclcpp/rclcpp.hpp>
 
 mbf_mesh_nav::MeshNavigationServer::Ptr mesh_nav_srv_ptr;
+rclcpp::Node::SharedPtr node;
 
 void sigintHandler(int sig)
 {
-  ROS_INFO_STREAM("Shutdown mesh navigation server.");
+  RCLCPP_INFO_STREAM(node->get_logger(), "Shutdown mesh navigation server.");
   if (mesh_nav_srv_ptr)
   {
     mesh_nav_srv_ptr->stop();
   }
-  ros::shutdown();
+  rclcpp::shutdown();
 }
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "mbf_mesh_nav", ros::init_options::NoSigintHandler);
+  rclcpp::init(argc, argv, rclcpp::InitOptions(), rclcpp::SignalHandlerOptions::None);
 
-  ros::NodeHandle nh;
-  ros::NodeHandle private_nh("~");
+   node = std::make_shared<rclcpp::Node>("mbf_mesh_nav");
+  const double cache_time = node->declare_parameter("tf_cache_time", 10.0);
 
-  double cache_time;
-  private_nh.param("tf_cache_time", cache_time, 10.0);
+  TFPtr tf_listener_ptr = std::make_shared<TF>(node->get_clock(), tf2::durationFromSec(cache_time));
+  tf2_ros::TransformListener tf_listener(*tf_listener_ptr);
+  mesh_nav_srv_ptr = std::make_shared<mbf_mesh_nav::MeshNavigationServer>(tf_listener_ptr, node);
 
   signal(SIGINT, sigintHandler);
-#ifdef USE_OLD_TF
-  TFPtr tf_listener_ptr(new TF(nh, ros::Duration(cache_time), true));
-#else
-  TFPtr tf_listener_ptr(new TF(ros::Duration(cache_time)));
-  tf2_ros::TransformListener tf_listener(*tf_listener_ptr);
-#endif
-  mesh_nav_srv_ptr = boost::make_shared<mbf_mesh_nav::MeshNavigationServer>(tf_listener_ptr);
-  ros::spin();
+
+  rclcpp::spin(node);
+
   return EXIT_SUCCESS;
 }
