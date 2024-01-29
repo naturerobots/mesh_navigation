c46a9dacbe49952391ce7a0f467d7eff0214d221
Matthias Holoch <mholoch@gmail.com>
2024-01-29 14:08:33 +0100

    fix methods defined in header

--- mesh_layers/include/mesh_layers/roughness_layer.h
+++ mesh_layers/include/mesh_layers/roughness_layer.h
@@ -108,7 +108,7 @@ class RoughnessLayer : public mesh_map::AbstractLayer
    */
   virtual std::set<lvr2::VertexHandle>& lethals() override
   {
-    return lethal_vertices;
+    return lethal_vertices_;
   }
 
   /**
@@ -117,7 +117,7 @@ class RoughnessLayer : public mesh_map::AbstractLayer
    * @param added_lethal vertices to be marked as lethal
    * @param removed_lethal vertices to be removed from the set of lethal vertices
    */
-  virtual void updateLethal(std::set<lvr2::VertexHandle>& added_lethal, std::set<lvr2::VertexHandle>& removed_lethal){} override;
+  virtual void updateLethal(std::set<lvr2::VertexHandle>& added_lethal, std::set<lvr2::VertexHandle>& removed_lethal) override {};
 
   /**
    * @brief initializes this layer plugin
