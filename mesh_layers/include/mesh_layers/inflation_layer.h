ce5e226c9b5754734ea1c8b19fbdfdf6ad9be1db
Alexander Mock <amock@uos.de>
2025-07-04 12:00:18 +0200

    cleaned up inflation layer. changed cost fading to standard costmap2d exponential term

--- mesh_layers/include/mesh_layers/inflation_layer.h
+++ mesh_layers/include/mesh_layers/inflation_layer.h
@@ -91,8 +91,8 @@ class InflationLayer : public mesh_map::AbstractLayer
    * @param inscribed_value value assigned to vertices in inscribed area
    * @param lethal_value value assigned to lethal vertices
    */
-  void lethalCostInflation(const std::set<lvr2::VertexHandle>& lethals, const float inflation_radius,
-                           const float inscribed_radius, const float inscribed_value, const float lethal_value);
+  // void lethalCostInflation(const std::set<lvr2::VertexHandle>& lethals, const float inflation_radius,
+  //                          const float inscribed_radius, const float inscribed_value, const float lethal_value);
 
   inline float computeUpdateSethianMethod(const float& d1, const float& d2, const float& a, const float& b,
                                           const float& dot, const float& F);
@@ -241,8 +241,9 @@ class InflationLayer : public mesh_map::AbstractLayer
     double inscribed_radius = 0.25;
     double inflation_radius = 0.4;
     double factor = 1.0;
-    double lethal_value = 2.0;
-    double inscribed_value = 1.0;
+    double lethal_value = 1.0;
+    double inscribed_value = 0.99;
+    double cost_scaling_factor = 1.0;
     int min_contour_size = 3;
     bool repulsive_field = true;
   } config_;
